fae80cad07dd0cde8a07deb435a11d7201026edc
Daniel Rodriguez <daniel.rodriguezti1@gmail.com>
2026-03-15 01:10:52 +0100

    nuevo concept de estilo

--- scripts/start_test_server.sh
+++ scripts/start_test_server.sh
@@ -7,6 +7,20 @@ PORT="${PORT:-8080}"
 
 cd "$ROOT_DIR"
 
+if [ ! -d "$ROOT_DIR/vendor" ]; then
+    echo "Instalando dependencias PHP..."
+    composer require scssphp/scssphp
+fi
+
+if [ ! -d "$ROOT_DIR/bootstrap-scss" ]; then
+    echo "Descargando Bootstrap SCSS..."
+    wget -q https://github.com/twbs/bootstrap/archive/refs/tags/v5.3.8.zip -O "$ROOT_DIR/bs.zip"
+    unzip -q "$ROOT_DIR/bs.zip" -d "$ROOT_DIR"
+    mv "$ROOT_DIR/bootstrap-5.3.8/scss" "$ROOT_DIR/bootstrap-scss"
+    rm -rf "$ROOT_DIR/bs.zip" "$ROOT_DIR/bootstrap-5.3.8"
+fi
+
+
 echo "Iniciando servidor de pruebas en http://$HOST:$PORT"
 echo "Root público: $ROOT_DIR/ctf"
 
